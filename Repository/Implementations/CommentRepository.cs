fa3f543964c2de84802b8a3365db584f33ce8d20
absyinka <absyinka@gmail.com>
2023-02-12 11:44:34 +0100

    Added comment repository interface and implementation

---
+++ Repository/Implementations/CommentRepository.cs
@@ -0,0 +1,14 @@
+﻿using IdealDiscuss.Context;
+using IdealDiscuss.Entities;
+using IdealDiscuss.Repository.Interfaces;
+
+namespace IdealDiscuss.Repository.Implementations
+{
+    public class CommentRepository : BaseRepository<Comment>, ICommentRepository
+    {
+        public CommentRepository(IdealDiscussContext context)
+        {
+            _context = context;
+        }
+    }
+}
