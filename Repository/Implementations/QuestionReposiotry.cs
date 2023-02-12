86c2428130a302e80b9705970e799beca024bb35
AbdulWaheedMahmoodAhmad <107749621+opemiposi123@users.noreply.github.com>
2023-02-12 09:59:43 +0100

    change QuestionReposiotory  to QuestionRepository

--- Repository/Implementations/QuestionReposiotry.cs
+++ Repository/Implementations/QuestionReposiotry.cs
@@ -7,38 +7,11 @@ using System.Diagnostics.Metrics;
 
 namespace IdealDiscuss.Repository.Implementations
 {
-    public class QuestionReposiotry : BaseRepository<Question>, IQuestionRepository
+    public class QuestionRepository : BaseRepository<Question>, IQuestionRepository
     {
-        public QuestionReposiotry(IdealDiscussContext context)
+        public QuestionRepository(IdealDiscussContext context)
         {
             _context = context;
         }
-
-        public List<Question> GetAllQuestionCount()
-        {
-            return _context.Questions.ToList();
-        }
-
-        public List<QuestionDto> GetAllQuestion()
-        {
-            return _context.Questions.Select(x => new QuestionDto
-            {
-                Id = x.Id,
-                QuestionText = x.QuestionText
-            }).ToList();
-        }
-
-        public QuestionDto GetQuestionDetail(int id) =>
-             _context.Questions
-                            .Where(x => x.Id == id)
-                            .Select(x => new QuestionDto
-                            {
-                                Id = x.Id,
-                                QuestionText = x.QuestionText,
-                                ImageUrl = x.ImageUrl,
-                                UserId = x.UserId,
-                                User = x.User
-                            })
-                            .FirstOrDefault();
     }
 }
--- Repository/Interfaces/IQuestionRepository.cs
+++ Repository/Interfaces/IQuestionRepository.cs
@@ -1,4 +1,4 @@
-﻿using IdealDiscuss.Dto;
+﻿
 using IdealDiscuss.Entities;
 using System.Threading.Tasks;
 
@@ -6,9 +6,5 @@ namespace IdealDiscuss.Repository.Interfaces
 {
     public interface IQuestionRepository : IRepository<Question>
     {
-        List<Question> GetAllQuestionCount();
-        //Question FindQuestionById(int id);
-        List<QuestionDto> GetAllQuestion();
-        QuestionDto? GetQuestionDetail(int id);
     }
 }
