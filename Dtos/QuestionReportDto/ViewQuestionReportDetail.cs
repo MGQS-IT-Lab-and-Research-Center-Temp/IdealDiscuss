8be13015a1adc2574cb0dba766d783946314f411
AbdulWaheedMahmoodAhmad <107749621+opemiposi123@users.noreply.github.com>
2023-02-13 09:39:23 +0100

    completed question report dto

---
+++ Dtos/QuestionReportDto/ViewQuestionReportDetail.cs
@@ -0,0 +1,19 @@
+﻿using IdealDiscuss.Entities;
+
+namespace IdealDiscuss.Dtos.QuestionReportDto
+{
+    public class ViewQuestionReportDetail
+    {
+        public int UserId { get; set; }
+        public User User { get; set; }
+        public int QuestionId { get; set; }
+        public Question Question { get; set; }
+        public string AdditionalComment { get; set; }
+        public int Id { get; set; }
+        public string CreatedBy { get; set; }
+        public string ModifiedBy { get; set; }
+        public DateTime DateCreated { get; set; }
+        public DateTime LastModified { get; set; }
+        public bool IsDeleted { get; set; }
+    }
+}
