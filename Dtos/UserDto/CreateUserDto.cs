df0db5d37142ccf77480b679c4417f46f80d98ee
AbdulWaheedMahmoodAhmad <107749621+opemiposi123@users.noreply.github.com>
2023-02-14 14:43:29 +0100

    remove some fields

--- Dtos/UserDto/CreateUserDto.cs
+++ Dtos/UserDto/CreateUserDto.cs
@@ -5,7 +5,6 @@
         public string UserName { get; set; }
         public string Password { get; set; }
         public string Email { get; set; }
-        public int Id { get; set; }
         public string CreatedBy { get; set; }
         public DateTime DateCreated { get; set; }
         public bool IsDeleted { get; set; }
--- Dtos/UserDto/ViewAllUserDto.cs
+++ Dtos/UserDto/ViewAllUserDto.cs
@@ -5,6 +5,7 @@ namespace IdealDiscuss.Dtos.UserDto
     {
         public string UserName { get; set; }
         public string Password { get; set; }
+        public int Id { get; set; }
         public string Email { get; set; }
     }
 }
