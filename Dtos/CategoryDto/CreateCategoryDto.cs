0562459cac0b54dc4548696dd04d6ee1aeffe8a3
AbdulWaheedMahmoodAhmad <107749621+opemiposi123@users.noreply.github.com>
2023-02-14 14:39:14 +0100

    remove some properties

--- Dtos/CategoryDto/CreateCategoryDto.cs
+++ Dtos/CategoryDto/CreateCategoryDto.cs
@@ -4,7 +4,7 @@
     {
         public string Name { get; set; }
         public string Description { get; set; }
-        public int Id { get; set; }
+ 
         public DateTime DateCreated { get; set; }
         public string CreatedBy { get; set; }
         public bool IsDeleted { get; set; }
--- Dtos/CategoryDto/ViewAllCategoryDto.cs
+++ Dtos/CategoryDto/ViewAllCategoryDto.cs
@@ -3,6 +3,7 @@
     public class ViewAllCategoryDto
     {
         public string Name { get; set; }
+        public int Id { get; set; }
         public string Description { get; set; }
     }
 }
