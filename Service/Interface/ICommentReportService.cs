dfa206ab523856011da173976a53ca4ab584d0e3
BalikisBamgbose <balikisbamgbose@gmail.com>
2023-02-15 16:35:32 +0100

    Newly added CommentReport and interface

--- Service/Interface/ICommentReportService.cs
+++ Service/Interface/ICommentReportService.cs
@@ -1,6 +1,16 @@
-﻿namespace IdealDiscuss.Service.Interface
+﻿using IdealDiscuss.Entities;
+using System.Collections;
+
+namespace IdealDiscuss.Service.Interface
 {
      public interface ICommentReportService
      {
+          CommentReport UserId(int userId);
+          CommentReport User(User user);
+          CommentReport CommentId(int CommentId);
+          CommentReport Comment(Comment comment);
+          CommentReport AditionalComment(string aditionalComment);
+          ICollection<CommentReportFlag> list(CommentReportFlag flag);
      }
 }
+
