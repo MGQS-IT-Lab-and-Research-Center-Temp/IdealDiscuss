f2d2904f9ba29d52c1bb5308c60d7dd154db3a38
faranasj <faranasj@gmail.com>
2023-02-18 11:10:02 +0100

    Added category service implementations and interface

--- Service/Implementations/CategoryService.cs
+++ Service/Implementations/CategoryService.cs
@@ -1,6 +1,24 @@
-﻿namespace IdealDiscuss.Service.Implementations
+﻿using IdealDiscuss.Dtos.CategoryDto;
+using IdealDiscuss.Entities;
+using IdealDiscuss.Service.Interface;
+
+namespace IdealDiscuss.Service.Implementations
 {
-    public class CategoryService
+    public class CategoryService : ICategoryService
     {
+        public void CreateCategory(CreateCategoryDto request)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateCategory(int id, UpdateCategoryDto updateCategoryDto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ViewCategory(int id, Category category)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
--- Service/Interface/ICategoryService.cs
+++ Service/Interface/ICategoryService.cs
@@ -1,6 +1,12 @@
-﻿namespace IdealDiscuss.Service.Interface
+﻿using IdealDiscuss.Dtos.CategoryDto;
+using IdealDiscuss.Entities;
+
+namespace IdealDiscuss.Service.Interface
 {
     public interface ICategoryService
     {
+        void CreateCategory(CreateCategoryDto request);
+        void UpdateCategory(int id, UpdateCategoryDto updateCategoryDto);
+        void ViewCategory(int id, Category category);
     }
 }
