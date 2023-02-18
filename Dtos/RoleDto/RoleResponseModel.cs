ef8a5dd64d559b2b30fa0980e83a1b9e37dc67af
AbdulWaheedMahmoodAhmad <107749621+opemiposi123@users.noreply.github.com>
2023-02-18 21:25:17 +0100

    completed role service implementation

---
+++ Dtos/RoleDto/RoleResponseModel.cs
@@ -0,0 +1,14 @@
+﻿using IdealDiscuss.Dtos.CommentReport;
+
+namespace IdealDiscuss.Dtos.RoleDto
+{
+    public class RoleResponseModel : BaseResponseModel
+    {
+        public ViewRoleDto Role { get; set; }
+    }
+
+    public class RolesResponseModel : BaseResponseModel
+    {
+        public List<ViewRoleDto> Role { get; set; }
+    }
+}
---
+++ Service/Interface/IRoleService.cs
@@ -0,0 +1,15 @@
+﻿using IdealDiscuss.Dtos.CommentReport;
+using IdealDiscuss.Dtos;
+using IdealDiscuss.Dtos.RoleDto;
+
+namespace IdealDiscuss.Service.Interface
+{
+    public interface IRoleService
+    {
+        BaseResponseModel CreateRole(CreateRoleDto createRoleDto);
+        BaseResponseModel DeleteRole(int roleId);
+        BaseResponseModel UpdateRole(int roleId, UpdateRoleDto updateRoelDto);
+        RoleResponseModel GetRole(int roleId);
+        RolesResponseModel GetAllRole();
+    }
+}
