7cb457c479b553f11d71912f20099f9e17a183c1
abdulhaseebh <abdulhaseebakintola@gmail.com>
2023-02-19 14:10:49 +0100

    FlagService And Implementation

---
+++ Service/Interface/IFlagService.cs
@@ -0,0 +1,14 @@
+﻿using IdealDiscuss.Dtos.FlagDto;
+using IdealDiscuss.Dtos;
+
+namespace IdealDiscuss.Service.Interface
+{
+    public interface IFlagService
+    {
+        BaseResponseModel CreateFlag(CreateFlagDto createFlagDto);
+        BaseResponseModel DeleteFlag(int flagId);
+        BaseResponseModel UpdateFlag(int flagId, UpdateFlagDto FlagDto);
+        FlagResponseModel GetFlag(int flagId);
+        FlagsResponseModel GetAllFlag();
+    }
+}
