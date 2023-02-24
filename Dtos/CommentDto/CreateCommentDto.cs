df3c39e2b516e902bc4061e056ec4ee93efa6f1d
solvestation <44468255+absyinka@users.noreply.github.com>
2023-02-24 22:41:02 +0100

    Update CreateCommentDto.cs

--- Dtos/CommentDto/CreateCommentDto.cs
+++ Dtos/CommentDto/CreateCommentDto.cs
@@ -5,10 +5,7 @@ namespace IdealDiscuss.Dtos.CommentDto
     public class CreateCommentDto
     {
         public int UserId { get; set; }
-        public int QuestionId { get; set; }
-        public User User { get; set; }
-        public Question Question { get; set; }
-    
+        public int QuestionId { get; set; }    
         public string CommentText { get; set; }
     }
 }
