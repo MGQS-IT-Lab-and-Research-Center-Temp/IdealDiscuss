9090b406c0e51d3b0fcefd995ad09cf9c6b72ba2
intisor <abdulawwalintisor777@gmail.com>
2023-03-04 22:28:41 +0100

    Create Category

--- Controllers/CategoryController.cs
+++ Controllers/CategoryController.cs
@@ -1,15 +1,23 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using IdealDiscuss.Service.Interface;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace IdealDiscuss.Controllers
 {
     public class CategoryController : Controller
     {
-        
-        public ActionResult Index()
-        {
-            return View();
-        }
+        private readonly ICategoryService _categoryService;
+        private readonly ILogger<CategoryController> _logger;
 
+        public CategoryController(ILogger<CategoryController> logger, ICategoryService categoryService)
+        {
+            _categoryService = categoryService; 
+            _logger = logger;   
+        }
+        public IActionResult Index()
+        {
+            var categories = _categoryService.GetAllCategory();
+            return View(categories.Data);
+        }
     }
 }
