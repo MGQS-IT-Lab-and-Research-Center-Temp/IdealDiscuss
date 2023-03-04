ecb273763dc06391ff8217758596030956d1621a
= <abdussultan14@gmail.com>
2023-03-04 21:15:14 +0100

    Created "DeleteCategory" in CategoryController

--- Controllers/CategoryController.cs
+++ Controllers/CategoryController.cs
@@ -5,10 +5,27 @@ namespace IdealDiscuss.Controllers
 {
     public class CategoryController : Controller
     {
-        
+        private readonly ICategoryService _categoryService;
+        private readonly ILogger<CategoryController> _logger;
+
+        public CategoryController(ILogger<CategoryController> logger, ICategoryService categoryService)
+        {
+            _logger = logger;
+            _categoryService = categoryService;
+        }
+
         public ActionResult Index()
         {
             return View();
+        }
+
+        [HttpPost("category/{id}/delete")]
+        public IActionResult DeleteCategory([FromRoute] int id)
+        {
+            var response = _categoryService.DeleteCategory(id);
+            ViewBag.Message = response.Message;
+            ViewBag.Status = response.Status;
+            return RedirectToAction("Index", "Category");
         }
 
     }
