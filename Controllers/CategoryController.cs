0641df14084b5b85800c814d3dfc21e0e286a94b
BalikisBamgbose <balikisbamgbose@gmail.com>
2023-03-04 22:39:31 +0100

    Read One Category Controller and View

--- Controllers/CategoryController.cs
+++ Controllers/CategoryController.cs
@@ -1,15 +1,27 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using IdealDiscuss.Service.Implementations;
+using IdealDiscuss.Service.Interface;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace IdealDiscuss.Controllers
 {
     public class CategoryController : Controller
     {
-        
-        public ActionResult Index()
-        {
-            return View();
-        }
+        private readonly ICategoryService _categoryService;
+        private readonly ILogger<CategoryController> _logger;
+
+         public CategoryController(ILogger<CategoryController> logger, ICategoryService categoryService)
+         {
+            _logger = logger;
+            _categoryService = categoryService;
+         }
+            [HttpPost]
+            public IActionResult GetCategory(int id)
+            {
+                var response = _categoryService.GetCategory(id);
+                ViewBag.Message = response.Message;
+                return View(response.Data);
+            }
 
     }
 }
