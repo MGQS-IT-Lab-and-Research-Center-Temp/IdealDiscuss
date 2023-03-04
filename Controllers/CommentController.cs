a96060069337fd1112d4cd5eb1e852d19db0e9c8
Olarewaju34 <mohmzyo@gmail.com>
2023-03-04 19:01:46 +0100

    CRUD ImplementationForComment

--- Controllers/CommentController.cs
+++ Controllers/CommentController.cs
@@ -6,78 +6,50 @@ namespace IdealDiscuss.Controllers
     public class CommentController : Controller
     {
         // GET: CommentController
-        public ActionResult Index()
+        public IActionResult Index()
         {
             return View();
         }
 
         // GET: CommentController/Details/5
-        public ActionResult Details(int id)
+        public IActionResult Details(int id)
         {
             return View();
         }
 
         // GET: CommentController/Create
-        public ActionResult Create()
+        [HttpGet]
+        public IActionResult Create()
         {
             return View();
         }
 
-        // POST: CommentController/Create
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Create(IFormCollection collection)
-        {
-            try
-            {
-                return RedirectToAction(nameof(Index));
-            }
-            catch
-            {
-                return View();
-            }
-        }
-
+   
         // GET: CommentController/Edit/5
-        public ActionResult Edit(int id)
+        public IActionResult Edit(int id)
         {
             return View();
         }
 
         // POST: CommentController/Edit/5
         [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Edit(int id, IFormCollection collection)
+       
+        public IActionResult Edit()
         {
-            try
-            {
-                return RedirectToAction(nameof(Index));
-            }
-            catch
-            {
-                return View();
-            }
+            return View();
         }
 
         // GET: CommentController/Delete/5
-        public ActionResult Delete(int id)
+        public IActionResult Delete(int id)
         {
             return View();
         }
 
         // POST: CommentController/Delete/5
         [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Delete(int id, IFormCollection collection)
+        public IActionResult Delete()
         {
-            try
-            {
-                return RedirectToAction(nameof(Index));
-            }
-            catch
-            {
-                return View();
-            }
+            return View();
         }
     }
 }
