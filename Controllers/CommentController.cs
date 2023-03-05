7d2ead2a573aaa0a30bbba8e9e3ddecc6037c0a1
Abdul rasheed Bello <akindats1@gmail.com>
2023-03-05 10:05:41 +0100

    Comment Updated

--- Controllers/CommentController.cs
+++ Controllers/CommentController.cs
@@ -1,5 +1,9 @@
+<<<<<<< HEAD
+﻿using IdealDiscuss.Service.Interface;
+=======
 ﻿using IdealDiscuss.Dtos.CommentDto;
 using IdealDiscuss.Service.Interface;
+>>>>>>> origin/CommentCreateController
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -10,6 +14,9 @@ namespace IdealDiscuss.Controllers
         private readonly ICommentService _commentService;
         private readonly ILogger<CommentController> _logger;
 
+<<<<<<< HEAD
+        public CommentController(ICommentService commentService,ILogger<CommentController> logger)
+=======
         public CommentController(ILogger<CommentController> logger, ICommentService commentService)
         {
             _logger = logger;
@@ -17,22 +24,44 @@ namespace IdealDiscuss.Controllers
         }
         //GET: CommentController
         public ActionResult Index()
+>>>>>>> origin/CommentCreateController
         {
-            return View();
+            _commentService = commentService;
+            _logger = logger;  
+        }
+        // GET: CommentController
+        public IActionResult Index()
+        {
+            var comments = _commentService.GetAllComment();
+            return View(comments.Comments);
         }
 
         // GET: CommentController/Details/5
-        public ActionResult Details(int id)
+        public IActionResult GetCommentDetail(int id)
         {
-            return View();
+            var response = _commentService.GetComment(id);
+
+            ViewBag.Message = response.Message;
+            ViewBag.Status = response.Status;
+
+            return View(response.Comment);
         }
 
         // GET: CommentController/Create
+<<<<<<< HEAD
+        [HttpGet]
+=======
+>>>>>>> origin/CommentCreateController
         public IActionResult Create()
         {
             return View();
         }
 
+<<<<<<< HEAD
+   
+        // GET: CommentController/Edit/5
+        public IActionResult Edit(int id)
+=======
         // POST: CommentController/Create
         [HttpPost]
         public IActionResult Create(CreateCommentDto request)
@@ -45,26 +74,41 @@ namespace IdealDiscuss.Controllers
 
         //GET: CommentController/Edit/5
         public ActionResult Edit(int id)
+>>>>>>> origin/CommentCreateController
         {
             return View();
         }
 
         // POST: CommentController/Edit/5
         [HttpPost]
+<<<<<<< HEAD
+       
+        public IActionResult Edit()
+=======
         public ActionResult Edit(int id, IFormCollection collection)
+>>>>>>> origin/CommentCreateController
         {
             return View();
         }
 
+<<<<<<< HEAD
+        // GET: CommentController/Delete/5
+        public IActionResult Delete(int id)
+=======
         //GET: CommentController/Delete/5
         public ActionResult Delete(int id)
+>>>>>>> origin/CommentCreateController
         {
             return View();
         }
 
         // POST: CommentController/Delete/5
         [HttpPost]
+<<<<<<< HEAD
+        public IActionResult Delete()
+=======
         public ActionResult Delete(int id, IFormCollection collection)
+>>>>>>> origin/CommentCreateController
         {
             return View();
         }
