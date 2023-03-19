ee72c4f11fd575dd66d3aee2963f1fb8afd29e8f
Abdul rasheed Bello <akindats1@gmail.com>
2023-03-19 15:33:04 +0100

    Comment Service Implemented

--- Controllers/CommentController.cs
+++ Controllers/CommentController.cs
@@ -3,6 +3,7 @@ using IdealDiscuss.Dtos.CommentDto;
 using Microsoft.AspNetCore.Mvc;
 using IdealDiscuss.Service.Implementations;
 using System.Security.Claims;
+using Microsoft.AspNetCore.Authorization;
 
 namespace IdealDiscuss.Controllers
 {
@@ -17,7 +18,8 @@ namespace IdealDiscuss.Controllers
             _httpContextAccessor = httpContextAccessor;
             _commentService = commentService;
         }
-       
+
+        [Authorize(Roles = "Admin")]
         public IActionResult Index()
         {
             var comments = _commentService.GetAllComment();
@@ -68,6 +70,7 @@ namespace IdealDiscuss.Controllers
             return RedirectToAction("Index", "Comment");
         }
 
+        [Authorize(Roles ="Admin")]
         [HttpPost("comment/{id}/delete")]
         public IActionResult DeleteComment([FromRoute] int id)
         {
