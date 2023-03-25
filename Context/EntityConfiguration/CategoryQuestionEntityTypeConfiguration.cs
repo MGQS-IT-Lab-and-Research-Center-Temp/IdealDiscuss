904ca801fd140f896a1fa13cd8cea6f61d1ba768
Oni Bashir Muh'Bashir <108095967+Muhbashir2005@users.noreply.github.com>
2023-03-25 18:23:33 +0100

    Created File CategoryQuestionEntityTypeConfiguration

---
+++ Context/EntityConfiguration/CategoryQuestionEntityTypeConfiguration.cs
@@ -0,0 +1,13 @@
+﻿using IdealDiscuss.Entities;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
+
+namespace IdealDiscuss.Context.EntityConfiguration
+{
+	public class CategoryQuestionEntityTypeConfiguration : 
+	{
+
+		
+	}
+
+}
