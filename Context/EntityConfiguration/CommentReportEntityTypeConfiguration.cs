d07405732407d1df7250821c69207bf25d4c6959
faranasj <faranasj@gmail.com>
2023-03-25 20:08:31 +0100

    Added CommentReportEntityTypeConfiguration

---
+++ Context/EntityConfiguration/CommentReportEntityTypeConfiguration.cs
@@ -0,0 +1,23 @@
+﻿using IdealDiscuss.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace IdealDiscuss.Context.EntityConfiguration
+{
+    public class CommentReportEntityTypeConfiguration : IEntityTypeConfiguration<CommentReport>
+    {
+        public void Configure(EntityTypeBuilder<CommentReport> builder)
+        {
+            builder.ToTable("CommentReports");
+            builder.Property(c => c.CommentReportFlags)
+                .IsRequired();
+            builder.HasOne(c => c.User)
+                .WithMany()
+                .HasForeignKey(c => c.UserId);
+            builder.HasOne(c => c.Comment)
+                .WithMany()
+                .HasForeignKey(c => c.CommentId);
+        }
+    }
+}
+
