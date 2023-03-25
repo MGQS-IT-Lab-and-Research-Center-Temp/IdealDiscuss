baacd2678796f5dbda620856638db690b7cec4f4
Oni Bashir Muh'Bashir <108095967+Muhbashir2005@users.noreply.github.com>
2023-03-25 18:27:46 +0100

    Modified By adding .IsRequired

--- Context/EntityConfiguration/CategoryQuestionEntityTypeConfiguration.cs
+++ Context/EntityConfiguration/CategoryQuestionEntityTypeConfiguration.cs
@@ -13,10 +13,12 @@ namespace IdealDiscuss.Context.EntityConfiguration
 			builder.HasKey(cq => new { cq.CategoryId, cq.QuestionId });
 			builder.HasOne(cq => cq.Category)
 				.WithMany(c => c.CategoryQuestions)
-				.HasForeignKey(cq => cq.CategoryId);
+				.HasForeignKey(cq => cq.CategoryId)
+				.IsRequired();
 			builder.HasOne(cq => cq.Question)
 				.WithMany(q => q.CategoryQuestions)
-				.HasForeignKey(cq => cq.QuestionId);
+				.HasForeignKey(cq => cq.QuestionId)
+				.IsRequired();
 		}
 	}
 
