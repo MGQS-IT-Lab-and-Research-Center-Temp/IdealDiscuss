4decc4b5276e405f233fafb83e8c6660463872e9
= <abdussultan14@gmail.com>
2023-03-25 19:02:31 +0100

    Role Entity Type Configuration

---
+++ Context/EntityConfiguration/RoleEntityTypeConfiguration.cs
@@ -0,0 +1,20 @@
+﻿using IdealDiscuss.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace IdealDiscuss.Context.EntityConfiguration
+{
+    public class CategoryEntityTypeConfiguration : IEntityTypeConfiguration<Category>
+    {
+        public void Configure(EntityTypeBuilder<Category> builder)
+        {
+            builder.ToTable("Categories");
+            builder.HasKey(c => c.Id);
+            builder.Property(c => c.Name)
+                .IsRequired()
+                .HasMaxLength(15);
+            builder.HasIndex(c => c.Name)
+             .IsUnique();
+        }
+    }
+}
