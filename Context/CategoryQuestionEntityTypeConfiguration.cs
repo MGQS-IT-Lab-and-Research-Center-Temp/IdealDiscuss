3db393f91b4fa1001a1234d888fe4deab0ba50e0
Oni Bashir Muh'Bashir <108095967+Muhbashir2005@users.noreply.github.com>
2023-03-25 17:12:10 +0100

    Finished task

--- Context/CategoryQuestionEntityTypeConfiguration.cs
+++ Context/CategoryQuestionEntityTypeConfiguration.cs
@@ -10,7 +10,12 @@ namespace IdealDiscuss.Context
 		{
 			builder.ToTable("CategoryQuestions");
 			builder.HasKey(cq => new { cq.CategoryId, cq.QuestionId });
-
+			builder.HasOne(cq => cq.Category)
+				.WithMany(c => c.CategoryQuestions)
+				.HasForeignKey(cq => cq.CategoryId);
+				builder.HasOne(cq => cq.Question)
+				.WithMany(q => q.CategoryQuestions)
+				.HasForeignKey(cq => cq.QuestionId);
 		}
 	}
 }
