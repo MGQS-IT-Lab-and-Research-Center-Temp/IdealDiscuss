ff33d8689d624bfb162a3ca02bf24a8eeb49ef0f
Oni Bashir Muh'Bashir <108095967+Muhbashir2005@users.noreply.github.com>
2023-03-25 18:26:35 +0100

    Added Configure Method

--- Context/EntityConfiguration/CategoryQuestionEntityTypeConfiguration.cs
+++ Context/EntityConfiguration/CategoryQuestionEntityTypeConfiguration.cs
@@ -6,8 +6,18 @@ namespace IdealDiscuss.Context.EntityConfiguration
 {
 	public class CategoryQuestionEntityTypeConfiguration : IEntityTypeConfiguration<CategoryQuestion>
 	{
+		public void Configure(EntityTypeBuilder<CategoryQuestion> builder)
+		{
+			builder.ToTable("CategoryQuestions");
 
-		
+			builder.HasKey(cq => new { cq.CategoryId, cq.QuestionId });
+			builder.HasOne(cq => cq.Category)
+				.WithMany(c => c.CategoryQuestions)
+				.HasForeignKey(cq => cq.CategoryId);
+			builder.HasOne(cq => cq.Question)
+				.WithMany(q => q.CategoryQuestions)
+				.HasForeignKey(cq => cq.QuestionId);
+		}
 	}
 
 }
