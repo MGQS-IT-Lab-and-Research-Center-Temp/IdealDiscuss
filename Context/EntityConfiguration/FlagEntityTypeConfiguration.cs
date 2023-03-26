9dda181a0a182c1e7425abfcd86e4d92ee479a9e
warith23 <ismailayomide32@gmail.com>
2023-03-26 05:48:15 +0100

    FlagEntityConfiguration

---
+++ Context/EntityConfiguration/FlagEntityTypeConfiguration.cs
@@ -0,0 +1,20 @@
+using IdealDiscuss.Entities;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
+
+namespace IdealDiscuss.Context.EntityConfiguration
+{
+    public class FlagEntityConfiguration : IEntityTypeConfiguration<Flag>
+    {
+        public void Configure(EntityTypeBuilder<Flag> builder)
+		{
+			builder.ToTable("Flags");
+			builder.HasKey(f => f.Id);
+            builder.Property(f => f.FlagName)
+                .IsRequired()
+                .HasMaxLength(15);
+            builder.HasIndex(f => f.FlagName)
+             .IsUnique();
+		}
+    }
+}
