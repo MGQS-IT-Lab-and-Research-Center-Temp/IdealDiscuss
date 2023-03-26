5fddedd8f8cd25cffb5cae9d3fd6964c44986cd5
BalikisBamgbose <balikisbamgbose@gmail.com>
2023-03-26 01:10:34 +0100

    QuestionReportConfig

---
+++ Context/EntityConfiguration/QuestionReportEntityTypeConfiguration.cs
@@ -0,0 +1,26 @@
+﻿using IdealDiscuss.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace IdealDiscuss.Context
+{
+    public class QuestionReportEntityTypeConfiguration : IEntityTypeConfiguration<QuestionReport>
+    {
+        public void Configure(EntityTypeBuilder<QuestionReport> builder)
+        {
+            builder.ToTable("QuestionReport");
+            builder.HasKey(qr => new { qr.UserId, qr.QuestionId });
+                    
+            builder.Property(qr => qr.AdditionalComment).HasColumnType("varchar(50)")
+                    .IsRequired();
+           builder.HasKey(qr => new { qr.QuestionId });
+            builder.HasOne(qr => qr.Question);
+            builder.HasOne(qr => qr.User);
+            
+
+
+
+
+        }
+    }
+}
