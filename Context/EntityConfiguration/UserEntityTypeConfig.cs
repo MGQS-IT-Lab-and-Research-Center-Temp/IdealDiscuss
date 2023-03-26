d19f6b94b0a1a8802e662ad3e4100811339ebf1c
Munzir32 <adeniranm07@gmail.com>
2023-03-26 07:52:31 +0100

    UserEntityTypeConfig added

---
+++ Context/EntityConfiguration/UserEntityTypeConfig.cs
@@ -0,0 +1,30 @@
+﻿using IdealDiscuss.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace IdealDiscuss.Context.EntityConfiguration
+{
+    public class UserEntityTypeConfig : IEntityTypeConfiguration<User>
+	{
+		
+
+        public void Configure(EntityTypeBuilder<User> builder)
+        {
+			
+				builder.ToTable("Users");
+			builder.HasKey(u => new { u.UserName, u.RoleId });
+			builder.Property(u => u.UserName).IsRequired().HasMaxLength(10);
+			builder.Property(u => u.Email).IsRequired();
+			builder.Property(u => u.Role).IsRequired();
+			builder.HasOne(u => u.Role)
+			.WithMany(r => r.Users)
+            .HasForeignKey(u => u.RoleId);
+				
+			
+				
+				
+			
+			
+        }
+    }
+}
