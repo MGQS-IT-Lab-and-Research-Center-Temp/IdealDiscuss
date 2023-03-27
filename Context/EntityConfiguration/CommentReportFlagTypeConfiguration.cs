38b793b95a895f323684085c7ad53b54a4d6ee01
AbdulWaheed MahmoodAhmad <abdulwaheedmahmoodahmad@gmail.com>
2023-03-27 22:39:40 +0100

    comment report flag type config

---
+++ Context/EntityConfiguration/CommentReportFlagTypeConfiguration.cs
@@ -0,0 +1,24 @@
+﻿using IdealDiscuss.Entities;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
+
+namespace IdealDiscuss.Context.EntityConfiguration
+{
+	public class CommentReportFlagTypeConfiguration : IEntityTypeConfiguration<CommentReportFlag>
+	{
+		public void Configure(EntityTypeBuilder<CommentReportFlag> builder)
+		{
+			builder.ToTable("CommentReportFlags");
+			builder.HasKey(cq => new { cq.CommentReportId, cq.FlagId });
+			builder.HasOne(cq => cq.CommentReport)
+				.WithMany(c => c.CommentReportFlags)
+				.HasForeignKey(cq => cq.CommentReportId)
+				.IsRequired();
+			builder.HasOne(cq => cq.Flag)
+				.WithMany(q => q.CommentReportFlags)
+				.HasForeignKey(cq => cq.FlagId)
+				.IsRequired();
+		}
+	}
+
+}
