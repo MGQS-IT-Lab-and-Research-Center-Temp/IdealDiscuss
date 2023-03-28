9296d9aae35f29e292470fea0cfdce5dd2299c9f
faranasj <faranasj@gmail.com>
2023-03-28 17:44:23 +0100

    Added QuestionEntityTypeConfig

---
+++ Context/EntityConfiguration/QuestionEntityTypeConfiguration.cs
@@ -0,0 +1,23 @@
+﻿using IdealDiscuss.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace IdealDiscuss.Context.EntityConfiguration
+{
+    public class QuestionEntityTypeConfiguration : IEntityTypeConfiguration<Question>
+    {
+        public void Configure(EntityTypeBuilder<Question> builder)
+        {
+            builder.ToTable("Questions");
+
+            builder.HasKey(u => u.UserId);
+
+            builder.Property(q => q.QuestionText)
+                .IsRequired()
+                .HasMaxLength(150);
+
+            builder.HasMany(c => c.Comments)
+                   .WithOne(q => q.Question)
+                   .IsRequired();
+    }
+}
